05a35a970a204853fa1fb3077ff9b5004164793f
Sumita Lama <sumitalama@Sumitas-MacBook-Pro.local>
2024-10-03 19:20:36 -0400

    initail commit

--- src/components/topics/topic-list.tsx
+++ src/components/topics/topic-list.tsx
@@ -5,18 +5,18 @@ import paths from '@/path';
 
 
 export default async function TopicList(){
- const topics= await db.topic.findMany(); 
- const renderTopics= topics.map((Topicist)=>{
-return <div key={Topicist.id}>
-    <Link href={paths.topicShow(Topicist.slug)}>
-    <Chip>
-        {Topicist.slug}
-    </Chip>
-    </Link>
+    const topic= await db.topic.findMany();
+    const renderTopic= topic.map((topicist)=>{
+        return(
+            <div key={topicist.id}>
+                <div>
+                    {topicist.slug}
+                </div>
+                <div>
+                   <p>{topicist.description}</p> 
+                </div>
+            </div>
 
-</div>
- })
- return(
-    <div>{renderTopics}</div>
- )
+        )
+    })
 }
